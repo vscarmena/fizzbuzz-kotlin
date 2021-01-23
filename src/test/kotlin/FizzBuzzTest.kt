facd23cd014e61ea18d0d22220a5cd1d51f9f20a
Víctor S. Carmena <vcarmena@gmail.com>
2021-01-23 13:31:11 +0100

    Test refactor with ApprovalTests

--- src/test/kotlin/FizzBuzzTest.kt
+++ src/test/kotlin/FizzBuzzTest.kt
@@ -1,47 +1,26 @@
-import org.junit.jupiter.api.BeforeAll
+import org.approvaltests.Approvals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
 import kotlin.test.assertEquals
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class FizzBuzzTest {
 
-    private lateinit var fizzBuzz: FizzBuzz
-
-    @BeforeAll
-    fun setUp() {
-        fizzBuzz = FizzBuzz()
+    @Test
+    fun `evaluate numbers to apply conversion`() {
+        Approvals.verifyAll(
+            intArrayOf(1, 2, 3, 4, 5, 6, 10, 15, 20, 30, 50, 98, 99, 100).toTypedArray(),
+            this::getGeneratedValue
+        )
     }
 
-    @ParameterizedTest
-    @CsvSource("1, 1", "2, 2")
-    fun `evaluate regular numbers`(number: Int, expected: String) {
-        assertEquals(expected, fizzBuzz.generateValues()[number - 1])
-    }
-
-    @ParameterizedTest
-    @CsvSource("3", "6")
-    fun `evaluate numbers multiple of 3`(number: Int) {
-        assertEquals("Fizz", fizzBuzz.generateValues()[number - 1])
-    }
-
-    @ParameterizedTest
-    @CsvSource("5", "10")
-    fun `evaluate numbers multiple of 5`(number: Int) {
-        assertEquals("Buzz", fizzBuzz.generateValues()[number - 1])
-    }
-
-    @ParameterizedTest
-    @CsvSource("15", "30")
-    fun `evaluate numbers multiple of 3 and 5`(number: Int) {
-        assertEquals("FizzBuzz", fizzBuzz.generateValues()[number - 1])
+    private fun getGeneratedValue(number: Int): String {
+        return FizzBuzz().generateValues()[number - 1]
     }
 
     @Test
     fun `generate values to print`() {
-        assertEquals(100, fizzBuzz.generateValues().size)
+        assertEquals(100, FizzBuzz().generateValues().size)
     }
 
 }
