254c231a04296e8c0b27fb9495acdd105de3c6be
Víctor S. Carmena <vcarmena@gmail.com>
2021-01-23 02:25:24 +0100

    Refactor evaluate numbers multiple of 3

--- src/main/kotlin/FizzBuzz.kt
+++ src/main/kotlin/FizzBuzz.kt
@@ -1,7 +1,7 @@
 class FizzBuzz {
 
     fun evaluate(number: Int): String {
-        if (number == 3 || number == 6) {
+        if (number % 3 == 0) {
             return "Fizz"
         }
         return number.toString()
